cbf57bc88b55db8516a57229070c260cbab6956c
Max Dobbelstein <max.dobbelstein@gmail.com>
2025-03-08 18:16:48 +0100

    feat(): added validation for file size and file type

---
+++ TechChallenge.Services.InvoiceService/Validation/FileValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace TechChallenge.Services.InvoiceService.Validation;
+
+public class FileValidator : AbstractValidator<IFormFile>
+{
+    private const int MAXFILESIZE = 5242880;
+    private const string MIMETYPEPDF = "application/pdf"
+    public FileValidator()
+    {
+        RuleFor(x => x.Length).NotNull().LessThanOrEqualTo(MAXFILESIZE).WithMessage("Die Dateigröße darf 5 Megabyte nicht überschreiten");
+        RuleFor(x => x.ContentType).NotNull().Must(x => x.Equals(MIMETYPEPDF)).WithMessage("Es dürfen nur PDF Dateien hochgeladen werden");
+    }
+
+}
